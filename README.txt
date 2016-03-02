ba7c1d04b2218d3854b6734088c2832d75150f9d
Caleb Jones <self@calebjones.net>
2016-03-01 21:52:51 -0500

    Add multiple lines of input and quitting
    Read lines of input using fgets, into a 513 character buffer (to allow
    a max line length of 512 characters, plus the null terminator). Compare
    the line against the string "q\n" in order to choose whether or not to
    quit.
    
    Document the changes and limitations in the README

--- README.txt
+++ README.txt
@@ -6,4 +6,10 @@ Ed, Squared.
 As I'm sure you know, ed is the standard text editor.
 Because of this fact, I'm excited to use only ed to implement an ed clone.
 
+Limitations
+===========
+
+Lines in edsq are currently limited to 512 characters.
+Every possible input produces an error.
+
 ?
--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char **argv) {
-  puts("?");
+  while (true) {
+    char line_buffer[513];  // 512 characters and '\0'
+    if (fgets(line_buffer, 513, stdin) != NULL) {
+      if (line_buffer[0] == 'q' && line_buffer[1] == '\n') {
+        break;
+      }
+    }
+    puts("?");
+  }
   return 0;
 }
