d6568a4fe2170917bd79f6d0d4c9b12ed4f8d54a
Caleb Jones <self@calebjones.net>
2016-03-01 22:19:53 -0500

    Move line input into a seperate function

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/* precondition: out should be char[513] */
+bool get_line(char *out) {
+  return fgets(out, 513, stdin) != NULL;
+}
+
 int main(int argc, char **argv) {
   while (true) {
     char line_buffer[513];  // 512 characters and '\0'
-    if (fgets(line_buffer, 513, stdin) != NULL) {
+    if (get_line(line_buffer)) {
       if (line_buffer[0] == 'q' && line_buffer[1] == '\n') {
         break;
       }
