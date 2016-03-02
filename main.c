35234366f9628785087dcd9eaa767d5d0ba7d5d1
Caleb Jones <self@calebjones.net>
2016-03-01 22:47:13 -0500

    Add functions to strip whitespace
    In ed, commands like "   a    \n" are equivalent to "a\n", so we need
    a function that can merge them. Similarly, since every line ends with
    a newline, it's redundant to store them, and performing string
    comparisons that include them are slightly more annoying, so we need
    a function to strip those.

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 /* precondition: out should be char[513] */
 bool get_line(char *out) {
   return fgets(out, 513, stdin) != NULL;
+}
+
+void strip_newline(char *out) {
+  do {
+    if (*out == '\n') {
+      *out = '\0';
+    }
+  } while (*out++);
+}
+
+void strip_whitespace(char *out) {
+  char *scan = out;
+  while (*scan == ' ' && *scan != '\0') {
+    scan++;
+  }
+  char *first = scan;
+  char *last = scan;
+  while (*scan != '\0') {
+    if (*scan != ' ' && *scan != '\n') {
+      last = scan;
+    }
+    scan++;
+  }
+  int count = last - first + 1;
+  memmove(out, first, count);
+  out[count] = '\0';
 }
 
 int main(int argc, char **argv) {
