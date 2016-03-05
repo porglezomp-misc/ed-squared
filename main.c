7862a8baef730791e4c7806b7b63c2351133aa07
Caleb Jones <self@calebjones.net>
2016-03-05 12:39:58 -0500

    Add `p` command
    Print the current line if there is a current line
    Refactor appending text into a function

--- main.c
+++ main.c
@@ -20,23 +20,34 @@ bool get_command(char *out) {
   return true;
 }
 
+void append_text(buffer *buf) {
+  while (true) {
+    line *new_line = alloc_line();
+    get_line(new_line->text);
+    if (new_line->text[0] == '.' && new_line->text[1] == '\n' && new_line->text[2] == '\0') {
+      free_line(new_line);
+      break;
+    }
+    buffer_append_line(buf, new_line);
+  }
+}
+
+void print_line(buffer *buf) {
+  if (buf->current_line) fputs(buf->current_line->text, stdout);
+  else puts("?");
+}
+
 int main(int argc, char **argv) {
   char line_buffer[513];  /* 512 characters and '\0' */
   buffer *buf = alloc_buffer();
   while (true) {
     if (get_command(line_buffer)) {
       if (line_buffer[0] == 'a' && line_buffer[1] == '\0') {
-      while (true) {
-        line *new_line = alloc_line();
-        get_line(new_line->text);
-        if (new_line->text[0] == '.' && new_line->text[1] == '\n' && new_line->text[2] == '\0') {
-          free_line(new_line);
-          break;
-        }
-        buffer_append_line(buf, new_line);
-      }
+        append_text(buf);
       } else if (line_buffer[0] == 'q' && line_buffer[1] == '\0') {
         break;
+      } else if (line_buffer[0] == 'p' && line_buffer[1] == '\0') {
+        print_line(buf);
       } else {
         puts("?");
       }
